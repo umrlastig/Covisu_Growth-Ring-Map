f2ac21c36809f762226948692b3cbcadfdcc837f
jacques <jacques.gautier.9@gmail.com>
2022-05-18 17:25:39 +0200

    Update color_scale.js

--- color_scale.js
+++ color_scale.js
@@ -10,6 +10,17 @@ var color_scales_palettes = {
 		"10":['#f7fbff','#f7fbff','#deebf7','#c6dbef','#9ecae1','#6baed6','#4292c6','#2171b5','#08519c','#08306b'],		
 		"11":['#f7fbff','#f7fbff','#deebf7','#c6dbef','#9ecae1','#6baed6','#4292c6','#2171b5','#08519c','#08306b','#08306b']
 	},
+	uni_hue_blue_inverse:{
+		"3":['#fee0d2','#fc9272','#de2d26'],
+		"4":['#fee5d9','#fcae91','#fb6a4a','#cb181d'],
+		"5":['#fee5d9','#fcae91','#fb6a4a','#de2d26','#a50f15'],
+		"6":['#fee5d9','#fcbba1','#fc9272','#fb6a4a','#de2d26','#a50f15'],
+		"7":['#fee5d9','#fcbba1','#fc9272','#fb6a4a','#ef3b2c','#cb181d','#99000d'],
+		"8":['#fff5f0','#fee0d2','#fcbba1','#fc9272','#fb6a4a','#ef3b2c','#cb181d','#99000d'],
+		"9":['#fff5f0','#fee0d2','#fcbba1','#fc9272','#fb6a4a','#ef3b2c','#cb181d','#a50f15','#67000d'],
+		"10":['#fff5f0','#fff5f0','#fee0d2','#fcbba1','#fc9272','#fb6a4a','#ef3b2c','#cb181d','#a50f15','#67000d'],
+		"11":['#fff5f0','#fff5f0','#fee0d2','#fcbba1','#fc9272','#fb6a4a','#ef3b2c','#cb181d','#a50f15','#67000d','#67000d']
+	},
 	uni_hue_red:{
 		"3":['#fee0d2','#fc9272','#de2d26'],
 		"4":['#fee5d9','#fcae91','#fb6a4a','#cb181d'],
@@ -20,6 +31,17 @@ var color_scales_palettes = {
 		"9":['#fff5f0','#fee0d2','#fcbba1','#fc9272','#fb6a4a','#ef3b2c','#cb181d','#a50f15','#67000d'],
 		"10":['#fff5f0','#fff5f0','#fee0d2','#fcbba1','#fc9272','#fb6a4a','#ef3b2c','#cb181d','#a50f15','#67000d'],
 		"11":['#fff5f0','#fff5f0','#fee0d2','#fcbba1','#fc9272','#fb6a4a','#ef3b2c','#cb181d','#a50f15','#67000d','#67000d']
+	},
+	uni_hue_red_inverse:{
+		"3":['#deebf7','#9ecae1','#3182bd'],
+		"4":['#eff3ff','#bdd7e7','#6baed6','#2171b5'],
+		"5":['#eff3ff','#bdd7e7','#6baed6','#3182bd','#08519c'],
+		"6":['#eff3ff','#c6dbef','#9ecae1','#6baed6','#3182bd','#08519c'],
+		"7":['#eff3ff','#c6dbef','#9ecae1','#6baed6','#4292c6','#2171b5','#084594'],		
+		"8":['#f7fbff','#deebf7','#c6dbef','#9ecae1','#6baed6','#4292c6','#2171b5','#084594'],		
+		"9":['#f7fbff','#deebf7','#c6dbef','#9ecae1','#6baed6','#4292c6','#2171b5','#08519c','#08306b'],		
+		"10":['#f7fbff','#f7fbff','#deebf7','#c6dbef','#9ecae1','#6baed6','#4292c6','#2171b5','#08519c','#08306b'],		
+		"11":['#f7fbff','#f7fbff','#deebf7','#c6dbef','#9ecae1','#6baed6','#4292c6','#2171b5','#08519c','#08306b','#08306b']
 	},
 	uni_hue_green_blue:{
 		"3":['#e0f3db','#a8ddb5','#43a2ca'],
@@ -32,6 +54,17 @@ var color_scales_palettes = {
 		"10":['#f7fcf0','#f7fcf0','#e0f3db','#ccebc5','#a8ddb5','#7bccc4','#4eb3d3','#2b8cbe','#0868ac','#084081'],
 		"11":['#f7fcf0','#f7fcf0','#e0f3db','#ccebc5','#a8ddb5','#7bccc4','#4eb3d3','#2b8cbe','#0868ac','#084081','#084081']
 	},
+	uni_hue_green_blue_inverse:{
+		"3":['#ffeda0','#feb24c','#f03b20'],
+		"4":['#ffffb2','#fecc5c','#fd8d3c','#e31a1c'],
+		"5":['#ffffb2','#fecc5c','#fd8d3c','#f03b20','#bd0026'],
+		"6":['#ffffb2','#fed976','#feb24c','#fd8d3c','#f03b20','#bd0026'],
+		"7":['#ffffb2','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#b10026'],
+		"8":['#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#b10026'],
+		"9":['#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026'],
+		"10":['#ffffcc','#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026'],
+		"11":['#ffffcc','#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026','#800026']
+	},
 	uni_hue_yellow_red:{
 		"3":['#ffeda0','#feb24c','#f03b20'],
 		"4":['#ffffb2','#fecc5c','#fd8d3c','#e31a1c'],
@@ -42,6 +75,17 @@ var color_scales_palettes = {
 		"9":['#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026'],
 		"10":['#ffffcc','#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026'],
 		"11":['#ffffcc','#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026','#800026']
+	},
+	uni_hue_yellow_red_inverse:{
+		"3":['#e0f3db','#a8ddb5','#43a2ca'],
+		"4":['#f0f9e8','#bae4bc','#7bccc4','#2b8cbe'],
+		"5":['#f0f9e8','#bae4bc','#7bccc4','#43a2ca','#0868ac'],
+		"6":['#f0f9e8','#ccebc5','#a8ddb5','#7bccc4','#43a2ca','#0868ac'],
+		"7":['#f0f9e8','#ccebc5','#a8ddb5','#7bccc4','#4eb3d3','#2b8cbe','#08589e'],
+		"8":['#f7fcf0','#e0f3db','#ccebc5','#a8ddb5','#7bccc4','#4eb3d3','#2b8cbe','#08589e'],
+		"9":['#f7fcf0','#e0f3db','#ccebc5','#a8ddb5','#7bccc4','#4eb3d3','#2b8cbe','#0868ac','#084081'],
+		"10":['#f7fcf0','#f7fcf0','#e0f3db','#ccebc5','#a8ddb5','#7bccc4','#4eb3d3','#2b8cbe','#0868ac','#084081'],
+		"11":['#f7fcf0','#f7fcf0','#e0f3db','#ccebc5','#a8ddb5','#7bccc4','#4eb3d3','#2b8cbe','#0868ac','#084081','#084081']
 	},
 	diverging_blue_white_red:{
 		"3":['#ef8a62','#f7f7f7','#67a9cf'],
@@ -54,6 +98,17 @@ var color_scales_palettes = {
 		"10":['#67001f','#b2182b','#d6604d','#f4a582','#fddbc7','#d1e5f0','#92c5de','#4393c3','#2166ac','#053061'],
 		"11":['#67001f','#b2182b','#d6604d','#f4a582','#fddbc7','#f7f7f7','#d1e5f0','#92c5de','#4393c3','#2166ac','#053061']
 	},
+	diverging_blue_white_red_inverse:{
+		"3":['#af8dc3','#f7f7f7','#7fbf7b'],
+		"4":['#7b3294','#c2a5cf','#a6dba0','#008837'],
+		"5":['#7b3294','#c2a5cf','#f7f7f7','#a6dba0','#008837'],
+		"6":['#762a83','#af8dc3','#e7d4e8','#d9f0d3','#7fbf7b','#1b7837'],
+		"7":['#762a83','#af8dc3','#e7d4e8','#f7f7f7','#d9f0d3','#7fbf7b','#1b7837'],
+		"8":['#762a83','#9970ab','#c2a5cf','#e7d4e8','#d9f0d3','#a6dba0','#5aae61','#1b7837'],
+		"9":['#762a83','#9970ab','#c2a5cf','#e7d4e8','#d9f0d3','#a6dba0','#5aae61','#1b7837'],
+		"10":['#40004b','#762a83','#9970ab','#c2a5cf','#e7d4e8','#d9f0d3','#a6dba0','#5aae61','#1b7837','#00441b'],
+		"11":['#40004b','#762a83','#9970ab','#c2a5cf','#e7d4e8','#f7f7f7','#d9f0d3','#a6dba0','#5aae61','#1b7837','#00441b']
+	},
 	diverging_blue_yellow_red:{
 		"3":['#fc8d59','#ffffbf','#91bfdb'],
 		"4":['#d7191c','#fdae61','#abd9e9','#2c7bb6'],
@@ -64,6 +119,17 @@ var color_scales_palettes = {
 		"9":['#d73027','#f46d43','#fdae61','#fee090','#ffffbf','#e0f3f8','#abd9e9','#74add1','#4575b4'],
 		"10":['#a50026','#d73027','#f46d43','#fdae61','#fee090','#e0f3f8','#abd9e9','#74add1','#4575b4','#313695'],
 		"11":['#a50026','#d73027','#f46d43','#fdae61','#fee090','#ffffbf','#e0f3f8','#abd9e9','#74add1','#4575b4','#313695']
+	},
+	diverging_blue_yellow_red_inverse:{
+		"3":['#af8dc3','#f7f7f7','#7fbf7b'],
+		"4":['#7b3294','#c2a5cf','#a6dba0','#008837'],
+		"5":['#7b3294','#c2a5cf','#f7f7f7','#a6dba0','#008837'],
+		"6":['#762a83','#af8dc3','#e7d4e8','#d9f0d3','#7fbf7b','#1b7837'],
+		"7":['#762a83','#af8dc3','#e7d4e8','#f7f7f7','#d9f0d3','#7fbf7b','#1b7837'],
+		"8":['#762a83','#9970ab','#c2a5cf','#e7d4e8','#d9f0d3','#a6dba0','#5aae61','#1b7837'],
+		"9":['#762a83','#9970ab','#c2a5cf','#e7d4e8','#d9f0d3','#a6dba0','#5aae61','#1b7837'],
+		"10":['#40004b','#762a83','#9970ab','#c2a5cf','#e7d4e8','#d9f0d3','#a6dba0','#5aae61','#1b7837','#00441b'],
+		"11":['#40004b','#762a83','#9970ab','#c2a5cf','#e7d4e8','#f7f7f7','#d9f0d3','#a6dba0','#5aae61','#1b7837','#00441b']
 	},
 	diverging_blue_yellow_green_red:{
 		"3":['#fc8d59','#ffffbf','#99d594'],
@@ -90,5 +156,35 @@ var color_scales_palettes = {
 	},
 	SM_Maxime_5:{
 		"12":['#cb181d','#fb6a4a','#fcae91','#fee5d9','#31a354','#a1d99b','#e5f5e0','#3182bd','#9ecae1','#deebf7','#c51b8a','#fa9fb5']
-	}
+	},
+	JB_phases_vague:{
+		//"5": ['#bfbfbf','#8dd3c7','#ffffb3','#bebada','#bfbfbf']
+		//"5":['#bfbfbf','#addd8e','#fdbb84','#9ecae1','#bfbfbf']
+		//"4":['#f2f0f7','#cbc9e2','#9e9ac8','#6a51a3']
+		//"5":['#fee5d9','#fcae91','#6baed6','#2171b5','FFFFFF']		// montée en rouge et descente en bleu, valeur chrnologique
+		"5":['#fcae91','#cb181d','#3182bd','#bdd7e7','#eff3ff']			// montée en rouge descente en bleu, valeur selon nb de cas
+		
+	},
+	JB_phases_vague_focus:{
+		"5":['#eff3ff','#fcae91','#de2d26','#fb6a4a','#08519c']
+	},
+	JB_phases_vague_pic:{
+		"4":['#bdd7e7','#6baed6','#cb181d','#fb6a4a']
+	},
+	
+	JB_multi_vagues:{
+		"4":['#a1d99b','#bcbddc','#756bb1', '#deebf7']
+	},
+	JB_evt_politique:{
+		"4":['#edf8e9','#bae4b3','#9e9ac8','#6a51a3']
+	},
+	JB_auto_vague:{
+		"4":['#bfbfbf','#9ecae1','#3182bd','#bfbfbf']
+	},
+
+	JB_auto_vague_3class:{
+		"5":['#bfbfbf','#1f78b4','#e31a1c','#33a02c','#bfbfbf']
+	},
+	
+	
 }
